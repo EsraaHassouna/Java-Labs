cc7ef23b70ce3eaf0f5191cfa18aa7c374f5a53f
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2023-01-24 16:35:23 +0200

    Labs

---
+++ javaLabOneOne.java
@@ -0,0 +1,11 @@
+public class LabScanFromCMD{
+	public static void main(String args[]){
+		int i=0;
+		for(i=0;i<args.length;i++){		
+			if(args[]>0)
+				System.out.println("true value "+args[i]);	
+			else 
+				System.out.println("No input");
+		}
+	}
+}
---
+++ javaLabOneTwo.java
@@ -0,0 +1,13 @@
+public class LabScanFromCMD{
+	public static void main(String args[]){
+		int i=0;
+		args[0] = Integer.parseInt(args[0]);
+		int size = args[0]
+		for(i=0;i<size;i++){		
+			if(args[]>0)
+				System.out.println("true value "+args[1]);	
+			else 
+				System.out.println("No input");
+		}
+	}
+}
