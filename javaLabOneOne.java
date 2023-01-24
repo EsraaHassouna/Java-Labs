0e5703a93a3a4846f8f45d304b0e7ac41eb93b3b
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2023-01-24 18:40:03 +0200

    lastUpdate

--- javaLabOneOne.java
+++ javaLabOneOne.java
@@ -1,8 +1,8 @@
-public class LabScanFromCMD{
+public class JavaLabOneOne{
 	public static void main(String args[]){
 		int i=0;
 		for(i=0;i<args.length;i++){		
-			if(args[]>0)
+			if(args.length>0 && args[0].equals("coreId"))
 				System.out.println("true value "+args[i]);	
 			else 
 				System.out.println("No input");
--- javaLabOneTwo.java
+++ javaLabOneTwo.java
@@ -1,11 +1,10 @@
-public class LabScanFromCMD{
+public class JavaLabOneTwo{
 	public static void main(String args[]){
 		int i=0;
-		args[0] = Integer.parseInt(args[0]);
-		int size = args[0]
+		int size = Integer.parseInt(args[0]);
 		for(i=0;i<size;i++){		
-			if(args[]>0)
-				System.out.println("true value "+args[1]);	
+			if(args.length>0)
+				System.out.println(args[1]);	
 			else 
 				System.out.println("No input");
 		}
