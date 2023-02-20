2f075c08114cbcf4d148f335d12098a9c9051137
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2023-02-20 02:41:26 +0200

    lab2

--- lab2/task3/src/task3/BinarySearchAlgo.java
+++ lab2/task3/src/task3/BinarySearchAlgo.java
@@ -1,37 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package task3;
 
-/**
- *
- * @author sheha
- */
 public class BinarySearchAlgo {
-    
-    void binarySearch(int[] arr, int search, int first, int last){
+    // first represents first index of array
+    // last represents last index of array
+    // search representa the element in which we will search
+    int binarySearch(int[] arr, int search, int first, int last){
         int mid = 0;
         /* first element of the array is greater than the last one
            so that we will go to the mid by getting the average 
            mid = (first + last)/2
         */
-        if(last > first){
-            mid = (first + last)/2;
+        if(last >= first){
+            arr[mid] = (first + last)/2;
             // the search will locate in the right side of the array
             if(search > arr[mid]){
                 //we will get new mid 
-                binarySearch(arr, search, mid+1, last);
-                
+                return binarySearch(arr, search, mid+1, last);               
             }            
-            
-            
+            else if (search < arr[mid]){
+            // the search will locate in the left side of the array
+                return binarySearch(arr, search, first, mid-1);
+            }
+            else if (search == arr[mid]){
+            //mid will be the number that we search for
+                return mid;
+            }    
         }
-
-
-
-
+        return -1;
     }
     
 }
--- lab2/task3/src/task3/Task3.java
+++ lab2/task3/src/task3/Task3.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package task3;
 
-/**
- *
- * @author sheha
- */
 public class Task3 {
 
     /**
@@ -16,6 +7,21 @@ public class Task3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] arr={10,20,30,40,50};
+        int search=40;
+        int n = arr.length;
+      
+        System.out.println("the array is : ");
+        for(int i=0;i<n;i++){
+         System.out.println(arr[i]+"");
+        }
+        BinarySearchAlgo x = new BinarySearchAlgo();
+        int result = x.binarySearch(arr, search, 0, n-1);
+        if (result== -1) 
+                System.out.print("elment not found");
+        else 
+            System.out.print("The indix of 40 : " + result);
+            
     }
     
 }
