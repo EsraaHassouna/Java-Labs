d5088005af3abf522b22e69769f64a1e290d4123
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2023-02-21 09:16:22 +0200

    lab2 task2

---
+++ lab2/task2/src/task2/SubstringRepeation.java
@@ -0,0 +1,16 @@
+
+package task2;
+
+public class SubstringRepeation {
+    
+    //function to count how many substring repeated
+    String subReapeat(){
+        String name = "Esraa Hassouna Saleh Hassouna";
+        int result=name.indexOf("Hassouna");
+        return name.substring(result);
+        
+        
+    }
+    
+    
+}
